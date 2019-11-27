b04722e374a28aaa51cd0d3fe77b3ef289c6c977
JonnyKong <jonnykong1996@gmail.com>
2019-11-27 10:50:29 -0800

    src: update skeleton code

---
+++ cs239/src/main/java/edu/ucla/cs/jonnykong/cs239/App.java
@@ -0,0 +1,13 @@
+package edu.ucla.cs.jonnykong.cs239;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
