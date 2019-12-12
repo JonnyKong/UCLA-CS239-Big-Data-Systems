9b937a7ece1d59a9d486e8e7e94006e1dcd502b8
zeroteb <zhangenbang1996@gmail.com>
2019-12-11 22:36:29 -0800

    Implement example trident state topology

---
+++ cs239/src/main/java/edu/ucla/cs/jonnykong/cs239/wordcount_topology/TridentStateTopology.java
@@ -0,0 +1,50 @@
+package edu.ucla.cs.jonnykong.cs239.wordcount_topology;
+
+import edu.ucla.cs.jonnykong.cs239.bolt.CouchInsertBolt;
+import edu.ucla.cs.jonnykong.cs239.bolt.CouchUpdateBolt;
+import edu.ucla.cs.jonnykong.cs239.mapper.CouchInsertMapper;
+import edu.ucla.cs.jonnykong.cs239.mapper.CouchUpdateMapper;
+import edu.ucla.cs.jonnykong.cs239.trident.state.CouchState;
+import edu.ucla.cs.jonnykong.cs239.trident.state.CouchStateFactory;
+import edu.ucla.cs.jonnykong.cs239.trident.state.CouchStateUpdater;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.trident.Stream;
+import org.apache.storm.trident.TridentTopology;
+import org.apache.storm.trident.state.StateFactory;
+import org.apache.storm.tuple.Fields;
+
+
+public class TridentStateTopology {
+    public static void main(String[] args) throws Exception {
+        String url = "http://127.0.0.1:5984";
+        String collectionName = "baseball";
+        CouchInsertMapper mapper = new CouchInsertMapper();
+        CouchState.Options options = new CouchState.Options()
+                .withUrl(url)
+                .withCollectionName(collectionName)
+                .withMapper(mapper);
+
+        WordReaderSpout spout = new WordReaderSpout();
+        TridentTopology topology = new TridentTopology();
+        Stream stream = topology.newStream("spout1", spout);
+
+        StateFactory factory = new CouchStateFactory(options);
+        stream.partitionPersist(factory, new Fields("word"),
+                new CouchStateUpdater(), new Fields());
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        LocalCluster localCluster = new LocalCluster();
+        try {
+            localCluster.submitTopology("trident", conf, topology.build());
+            Thread.sleep(10000);
+            localCluster.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+            localCluster.shutdown();
+        }
+    }
+}
